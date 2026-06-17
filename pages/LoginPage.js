2ef6e547ae6df0d980c932e33f67e61a1a7fc980
nirussss <nirusanjhowa@gmail.com>
2026-06-17 12:57:11 +0545

    seperate folder and files

---
+++ pages/LoginPage.js
@@ -0,0 +1,19 @@
+class LoginPage{
+    constructor(page){
+        this.page=page;
+        this.username=page.locator("input[name='username']");
+        this.password=page.locator("input[name='password']");
+        this.loginBtn=page.getByRole('button', { name: 'Login' });
+    }
+    async open(){
+        await this.page.goto('https://customer-portal.worldlink.com.np/eservice-login');
+    }
+
+    async login(user,pass){
+        await this.username.fill(user);
+        await this.password.fill(pass);
+        await this.loginBtn.click();
+
+    }
+}
+export default LoginPage;
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,39 +1,17 @@
-import { test, expect } from '@playwright/test';
+import {test,expect} from '@playwright/test';
+import {LoginPage} from '../pages/LoginPage';
 
-test('valid login creds', async ({ page }) => {
-
-  await page.goto('https://customer-portal.worldlink.com.np/eservice-login');
-
- 
-  await page.locator("input[name='username']").fill('aakashduwal');
-  await page.locator("input[name='password']").fill('Duwals@77');
-
-  // click login
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  // wait for page to respond after login attempt
-  await page.waitForTimeout(3000);
-
-
-  await expect(page).toHaveURL(/eservice-login/);
+test('valid login creds', async({page})=>{
+const login=new  LoginPage(page);
+await login.open();
+await login.Login("aakashduwal","Duwals@77");
+await expect(page).toHaveURL('https://customer-portal.worldlink.com.np/');
 });
 
+test('invalid login creds',async({page})=>{
+  const login=new  LoginPage(page);
+  await login.open();
+  await login.Login("incorrectduwal","Duwals@77");
+  await expect (page).toHaveText('Invalid username');
 
-test('login with incorrect creds', async ({ page }) => {
-
-  await page.goto('https://customer-portal.worldlink.com.np/eservice-login');
-
-  // fill wrong credentials
-  await page.locator("input[name='username']").fill('incorrectuser');
-  await page.locator("input[name='password']").fill('Password123');
-
-  // click login
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  // wait for response
-  await page.waitForTimeout(3000);
-
-  // ASSERTION (check error message if it appears)
-  await expect(page.getByText(/invalid/i)).toBeVisible();
-
-});
+})
