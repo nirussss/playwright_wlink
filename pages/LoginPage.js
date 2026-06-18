4aef13e1ac6ea65786c711bc39ba3013c7ae41f9
nirussss <nirusanjhowa@gmail.com>
2026-06-18 13:03:18 +0545

    loop fixed

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,19 +1,23 @@
-class LoginPage{
-    constructor(page){
-        this.page=page;
-        this.username=page.locator("input[name='username']");
-        this.password=page.locator("input[name='password']");
-        this.loginBtn=page.getByRole('button', { name: 'Login' });
-    }
-    async open(){
-        await this.page.goto('https://customer-portal.worldlink.com.np/eservice-login');
-    }
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.username = page.locator("input[name='username']");
+    this.password = page.locator("input[name='password']");
+    this.loginBtn = page.getByRole('button', { name: 'Login' });
+  }
 
-    async login(user,pass){
-        await this.username.fill(user);
-        await this.password.fill(pass);
-        await this.loginBtn.click()
+  async open() {
+    await this.page.goto(
+      'https://customer-portal.worldlink.com.np/eservice-login',
+      { timeout: 60000 }
+    );
+  }
 
-    }
+  async login(user, pass) {
+    await this.username.fill(user);
+    await this.password.fill(pass);
+    await this.loginBtn.click();
+  }
 }
+
 export default LoginPage;
