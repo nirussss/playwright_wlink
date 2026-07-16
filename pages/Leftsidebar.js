e465a00f6f67f3f6304420a314e01dbdf204daaa
nirussss <nirusanjhowa@gmail.com>
2026-07-16 07:11:51 +0545

    userhelper modified

---
+++ pages/Leftsidebar.js
@@ -0,0 +1,5 @@
+import {expect } from '@playwright/test';
+
+class Leftsidebar { 
+        
+}
--- utils/userHelper.js
+++ utils/userHelper.js
@@ -2,13 +2,9 @@ import loginData from '../data/LoginData.json';
 
 export function getRandomValidUser() {
 
-    const validUsers = loginData.filter(
-        user => user.type === "valid"
-    );
+    const validUsers = loginData.filter(user => user.type === "valid");
 
-    const randomIndex = Math.floor(
-        Math.random() * validUsers.length
-    );
+    const randomIndex = Math.floor(Math.random() * validUsers.length);
 
     return validUsers[randomIndex];
 }
